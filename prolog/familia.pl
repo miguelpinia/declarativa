e6e6e0522666ee3f82b65c5f2e11511f500bd814
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2019-01-26 21:20:08 -0600

    Agrega el programa de la familia.

---
+++ prolog/familia.pl
@@ -0,0 +1,32 @@
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+
+female(pam).
+male(tom).
+male(bob).
+female(liz).
+female(ann).
+female(pat).
+male(jim).
+
+offspring(Y, X) :- parent( X, Y).
+
+mother(X, Y) :- parent(X, Y),
+                female(X).
+
+
+grandparent(X, Z) :- parent(X, Y),
+                     parent(Y, Z).
+
+sister( X, Y) :- parent(Z, X),
+                 parent(Z, Y),
+                 female(X),
+                 different(X, Y).
+
+predecessor(X, Z) :- parent( X, Z).
+predecessor( X, Z) :- parent( X, Y),
+                      predecessor(Y, Z).
