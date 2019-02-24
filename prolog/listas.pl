792c58fa0561179da36858b6dc16361adcb1f6fb
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2019-02-24 12:29:52 -0600

    Actualiza el contenido de listas.

--- prolog/listas.pl
+++ prolog/listas.pl
@@ -1,5 +1,5 @@
-member(X, [X|Tail]).
-member(X, [Head|Tail]) :- member(X, Tail).
+%% member(X, [X|Tail]).
+%% member(X, [Head|Tail]) :- member(X, Tail).
 conc([], L, L).
 conc(L, [], L).
 conc([X|L1], L2, [X|L3]) :- conc(L1, L2, L3).
@@ -8,8 +8,12 @@ add(L, [], L).
 add(X, L, [X|L]).
 
 del(X, [X|Tail], Tail).
-del(X, [], []).
+del(_, [], []).
 del(X, [Y|Tail], [Y|NewTail]) :- del(X, Tail, NewTail).
 
+insert(X, L, BL) :- del(X, BL, L), BL \== L.
+sublist(S, L) :- conc(_, L2, L), cnc(S, _, L2).
 
-sublist(S, L) :- conc(_, L2, L), conc(S, _, L2).
+
+perm([], []).
+perm([X|L], P) :- perm(L, L1), insert(X, L1, P).
